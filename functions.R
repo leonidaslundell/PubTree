4d6d7389bfc8078f213d3a3589dbcb9da940d545
Leonidas Lundell <leonidas.lundell@gmail.com>
2021-07-30 12:34:31 +0200

    v1 done

--- functions.R
+++ functions.R
@@ -2,83 +2,88 @@
 #get structure of article
 
 getInfo <- function(id){
-  
   results <- getSummary(id)
-  # results <- keep
+  Sys.sleep(.34) #to avoid spamming the API
+  results[[1]]$citing <- unlist(results[[1]]$citing)#unlist due to rare issue with the API and R interaction 
+  results[[1]]$citations <- unlist(results[[1]]$citations)
   
-  bind <- function(id2){
-    if(is.null(results[[id2]]$citing))
-      results[[id2]]$citing <- NA
-    if(is.null(results[[id2]]$citations))
-      results[[id2]]$citations <- NA
-    
-    x <- rbind(
-      data.frame(from = id2,
-                 to = results[[id2]]$citing),
-      data.frame(from = id2,
-                 to = id2),
-      data.frame(from = results[[id2]]$citations,
-                 to = id2)
-    )
-    
-    x[!(is.na(x$from)|is.na(x$to)),]
+  if(is.null(results[[1]]$citing))
+    results[[1]]$citing <- "none"
+  if(is.null(results[[1]]$citations))
+    results[[1]]$citations <- "none"
+  
+  if (length(results[[1]]$citations) < 200) {
+    citations <- results[[1]]$citations[!is.na(as.numeric(results[[1]]$citations))]
+    citations <- getSummary(citations)
+  } else {
+    results[[1]]$citations <- sample(results[[1]]$citations, size = 200, replace = F)
+    citations <- getSummary(results[[1]]$citations)
+  }
+  Sys.sleep(.34) #to avoid spamming the API
+  if (length(results[[1]]$citing) < 200) {
+    citing <- results[[1]]$citing[!is.na(as.numeric(results[[1]]$citing))]
+    citing <- getSummary(citing)
+  } else {
+    results[[1]]$citing <- sample(results[[1]]$citing, size = 200, replace = F)
+    citing <- getSummary(results[[1]]$citing)
   }
   
-  resultsDF <- lapply(names(results), \(id){
-    bind(id)
-  })
-  resultsDF <- Reduce(rbind, resultsDF)
-  resultsDF$type <- "cited"
-  resultsDF$type[resultsDF$from == id] <- "citing"
+  resultsDF <- rbind(
+    data.frame(from = results[[1]]$citations,
+               to = id), #unlist since some API returns are list??
+    data.frame(from = id,
+               to = id),
+    data.frame(from = id,
+               to = results[[1]]$citing)
+  )
+  resultsDF <- resultsDF[!(resultsDF$from == "none" | resultsDF$to == "none"),]
+  
+  resultsDF$type <- "cited" #cited = query is being cited
+  resultsDF$type[resultsDF$to %in% unlist(results[[1]]$citing)] <- "citing"#citing = query is citing
   resultsDF$type[resultsDF$from == id & resultsDF$to == id] <- "root"
   resultsDF$type <- as.factor(resultsDF$type)
   
-  #if an article is cited extensively, take sample of 200... (eutils limit)
-  #also this wierd detour to get same vector length as the df to make net
-  if(nrow(resultsDF)>200){
-    x <- resultsDF[c(sample(which(resultsDF$type == "cited"),
-                            200 - sum(resultsDF$type != "cited")),
-                     which(resultsDF$type != "cited")),]  
-  }else{
-    x <- resultsDF
-  }
+  if(citations == "none")
+    citations <- NULL
+  if(citing == "none")
+    citing <- NULL
   
-  xx <- as.numeric(x$type)
-  xx[xx>2] <- 2
-  xx <- c(x[xx == 2, 2], x[xx == 1, 1])
+  resultsMeta <- c(citations, results, citing)
   
-  resultsRoot <- results[[1]]
-  
-  #second round
-  
-  results <- getSummary(id = xx)
-  resultsCit <- sapply(results, \(x) c(length(x$citations), length(x$citing)))
+  resultsCit <- sapply(resultsMeta, \(x){
+    c(length(x$citations), length(x$citing))
+  })
   rownames(resultsCit) <- c("citations", "citing")
   
   #meta data
-  resultsDFmeta <- data.frame(PMID=colnames(resultsCit))
+  resultsDFmeta <- data.frame(PMID=names(resultsMeta))
   
   resultsDFmeta$citations <- resultsCit["citations",]
   resultsDFmeta$citing <- resultsCit["citing",]
-  resultsDFmeta$title <- sapply(results, \(x) x$title)
-  resultsDFmeta$abstract <- sapply(results, \(x){
+  resultsDFmeta$title <- sapply(resultsMeta, \(x) x$title)
+  resultsDFmeta$abstract <- sapply(resultsMeta, \(x){
     if(length(x$abstract)==0)
       return("NA")
     return(x$abstract)
   })
-  resultsDFmeta$language <- sapply(results, \(x) x$language)
-  resultsDFmeta$journal <- sapply(results, \(x) x$journal)
-  resultsDFmeta$type <- sapply(results, \(x) x$type)
-  resultsDFmeta$author <- sapply(results, \(x) paste0(x$author, collapse = ", "))
-  resultsDFmeta$year <- sapply(results, \(x){
+  resultsDFmeta$language <- sapply(resultsMeta, \(x) x$language)
+  resultsDFmeta$journal <- sapply(resultsMeta, \(x) x$journal)
+  resultsDFmeta$type <- sapply(resultsMeta, \(x) paste0(x$type, collapse = "/"))
+  resultsDFmeta$author <- sapply(resultsMeta, \(x) paste0(x$author, collapse = ", "))
+  resultsDFmeta$year <- sapply(resultsMeta, \(x){
     if(length(x$year)==0)
       return("NA")
     return(x$year)
   })
   
+  resultsDFmeta$order <- "cited"#cited = article is being cited
+  resultsDFmeta$order[resultsDFmeta$PMID %in% unlist(results[[1]]$citing)] <- "citing"#citing = article is citing other
+  resultsDFmeta$order[resultsDFmeta$PMID == id] <- "root"
+  resultsDFmeta$order <- as.factor(resultsDFmeta$order)
+  
   ####################################################
   #author distance
-  resultsDFmeta$group <- authorGroup(results)
+  resultsDFmeta$group <- authorGroup(resultsMeta)
   
   return(list(df = resultsDF, meta = resultsDFmeta))
 }
@@ -90,156 +95,100 @@ authorGroup <- function(results){
   ji <- function(x, y){
     length(intersect(x, y))/length(unique(c(x, y)))
   }
-  
+
   xx <- matrix(NA, length(results), length(results))
   colnames(xx) <- rownames(xx) <- names(results)
-  
+
   for(x in colnames(xx)){
     for(y in colnames(xx)){
       xx[x, y] <- 1- ji(x = results[[x]]$author,
-                        y = results[[y]]$author) 
+                        y = results[[y]]$author)
     }
   }
   xx[is.na(xx)] <- 0
-  
-  xxDist <- hclust(as.dist(xx), method = "complete")
-  # rev(unique(xxDist$height))[2]
-  
-  xxGroup <- cutree(xxDist, h = rev(unique(xxDist$height))[2])
-  
-  xxGroupName <- names(table(xxGroup))[table(xxGroup)>1]
-  xxGroupTable <- table(xxGroup)
-  xxGroup[xxGroup %in% names(xxGroupTable)[!names(xxGroupTable) %in% xxGroupName]] <- 
-    "0"
-  
-  for(i in xxGroupName){
-   ii <- which(xxGroupName %in% i)
-     
-   xxGroup[xxGroup %in% xxGroupName[[ii]]] <- paste0(Reduce(intersect, lapply(results[xxGroup == i], \(x) x$author)),
-                                                     collapse = ", ")
+
+  if(any(xx != 0)){
+    xxDist <- hclust(as.dist(xx), method = "complete")
+    
+    xxGroup <- cutree(xxDist, h = rev(unique(xxDist$height))[2])
+    
+    xxGroupName <- names(table(xxGroup))[table(xxGroup)>1]
+    xxGroupTable <- table(xxGroup)
+    xxGroup[xxGroup %in% names(xxGroupTable)[!names(xxGroupTable) %in% xxGroupName]] <-
+      "none"
+    
+    for(i in xxGroupName){
+      ii <- which(xxGroupName %in% i)
+      
+      xxGroup[xxGroup %in% xxGroupName[[ii]]] <- paste0(Reduce(intersect, lapply(results[xxGroup == i], \(x) x$author)),
+                                                        collapse = ", ")
+    }
+  }else{
+    xxGroup <- rep("none", length(results))
   }
   
-  xxGroup[xxGroup == "0"] <- "none"
   
   return(xxGroup)
 }
-
-# ####################################################
-# #get cited
-# getCited <- function(id){
-#   citedQuery <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_citedin&id="
-#   cited <-
-#     xml2::read_xml(paste0(citedQuery, id)) |>
-#     xml2::as_list()
-#   cited <- cited$eLinkResult$LinkSet$LinkSetDb |> unlist()
-#   names(cited) <- NULL
-#   cited <- cited[-1:-2]
-#   cited
-# }
-
-####################################################
-#get cited
-# getCiting <- function(id){
-#   citingQuery <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_refs&id="
-#   citing <-
-#     xml2::read_xml(paste0(citingQuery, id)) |>
-#     xml2::as_list()
-#   citing <- citing$eLinkResult$LinkSet$LinkSetDb |> unlist()
-#   names(citing) <- NULL
-#   citing <- citing[-1:-2]
-#   citing
-# }
-
-# ####################################################
-# #get citing and citation and reviews
-# 
-# getCitation <- function(id){
-#   
-#   citingQuery <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&retmode=json"
-#   citingQuery <- paste(citingQuery, paste0("&id=",id, collapse = ""), collapse = "", sep = "")
-#   citingQuery <- paste0(citingQuery, "&tool=pubnet&email=leonidas.lundell@gmail.com")
-#   citing <- jsonlite::read_json(citingQuery)
-#   
-#   #fix the names for clarity
-#   for(i in 1:length(citing$linksets)){
-#     names(citing$linksets[[i]]$linksetdbs) <- sapply(citing$linksets[[i]]$linksetdbs, \(x) {
-#       x$linkname
-#     })
-#     citing$linksets[[i]]$linksetdbs <- sapply(citing$linksets[[i]]$linksetdbs, \(x){
-#       unlist(x$links)
-#     })
-#     names(citing$linksets)[i] <- citing$linksets[[i]]$ids[[1]]
-#     citing$linksets[[i]] <- citing$linksets[[i]]$linksetdbs
-#   }
-#   citing <- citing$linksets
-#   
-#   citing <- lapply(citing, \(x){
-#     x <- x[c("pubmed_pubmed_refs", "pubmed_pubmed_citedin")]
-#     names(x) <- c("out", "in")
-#     x
-#   })
-#   
-#   #get citations
-#   "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&retmode=xml&linkname=pubmed_pmc_refs&id=25605792"
-#   
-#   citing
-# }
 
 ####################################################
 #get the summary of articles
 
 getSummary <- function(id){
+  if(is.null(id)|length(id)==0){
+    return("none")
+  }
   summaryQuery <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&rettype=abstract&version=2.0&id="
   summaryQuery <- paste0(summaryQuery, paste0(id, collapse = ","))
   summaryQuery <- paste0(summaryQuery, "&tool=pubnet&email=leonidas.lundell@gmail.com")
-  
-  summaryRes <- read_xml(summaryQuery)
-  
+
+  summaryRes <- xml2::read_xml(summaryQuery)
+
   #loop over length of id extract the xml attrs
   language <- lapply(1:length(id), \(i){
-    xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//Language") |> xml_text()
+    xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//Language") |> xml2::xml_text()
   })
 
   year <- lapply(1:length(id), \(i){
-    xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//DateCompleted/Year") |> xml_text()
+    xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//PubDate/Year") |> xml2::xml_text()
   })
- 
+
   journal <- lapply(1:length(id), \(i){
-    xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//Title") |> xml_text()
+    xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//Title") |> xml2::xml_text()
   })
-  
+
   type <- lapply(1:length(id), \(i){
-    i <- xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//PublicationType") |> xml_text()
+    i <- xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//PublicationType") |> xml2::xml_text()
     if(any(grepl("Review", i))){
       return(i[grepl("Review", i)])
     }else{
       return("Article")
     }
   })
-  
+
   title <- lapply(1:length(id), \(i){
-    xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//ArticleTitle") |> xml_text()
+    xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//ArticleTitle") |> xml2::xml_text()
   })
-  
+
   abstract <- lapply(1:length(id), \(i){
-    xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//Abstract") |> xml_text()
+    xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//Abstract") |> xml2::xml_text()
   })
-  
+
   citing <- lapply(1:length(id), \(i){
-    ii <- xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//ReferenceList/Reference") |> as_list()
+    ii <- xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//ReferenceList/Reference/ArticleIdList/ArticleId") |> xml2::xml_text()
     if(length(ii)>0){
       return({
         sapply(ii, \(x){
-          x$ArticleIdList$ArticleId[[1]]
+          x[[1]]
         })
       })
     }else{
       return(NULL)
     }
   })
-  
+
   author <- lapply(1:length(id), \(i){
-    ii <- xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//AuthorList/Author") |> as_list()
+    ii <- xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//AuthorList/Author") |> xml2::as_list()
     if(length(ii)>0){
       return({
         sapply(ii, \(x){
@@ -247,22 +196,22 @@ getSummary <- function(id){
         })
       })
     }else{
-      return(NULL)
+      return("none")
     }
   })
-  
-  #get citations from elink 
-  
-  summaryQuery <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&retmode=xml&linkname=pubmed_pmc_refs&version=2.0&"
+
+  #get citations from elink
+
+  summaryQuery <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&retmode=xml&linkname=pubmed_pubmed_citedin&version=2.0&"
   summaryQuery <- paste0(summaryQuery, paste0("id=",id, collapse = "&"))
   summaryQuery <- paste0(summaryQuery, "&tool=pubnet&email=leonidas.lundell@gmail.com")
-  
+
   Sys.sleep(.34)
-  summaryRes <- read_xml(summaryQuery)
-  
+  summaryRes <- xml2::read_xml(summaryQuery)
+
   citations <- lapply(1:length(id), \(i){
     # print(i)
-    ii <- xml_child(summaryRes, search = i) |> xml_find_all(xpath = ".//Link") |> as_list()
+    ii <- xml2::xml_child(summaryRes, search = i) |> xml2::xml_find_all(xpath = ".//Link") |> xml2::as_list()
     if(length(ii)>0){
       return({
         sapply(ii, \(x){
@@ -270,10 +219,10 @@ getSummary <- function(id){
         })
       })
     }else{
-      return(NULL)
+      return("none")
     }
   })
-  
+
   out <- list(title = title,
               language = language,
               type = type,
@@ -283,18 +232,120 @@ getSummary <- function(id){
               author = author,
               citing = citing,
               citations = citations)
-  
+
   out <- lapply(out, \(x){
     names(x) <- id
     x
   })
-  
+
   out <- lapply(id, \(x){
     lapply(out, \(xx){
       xx[[x]]
     })
   })
   names(out) <- id
-  
+
   return(out)
+}
+
+####################################################
+#create igraph
+
+net <- function(citations, layout = "tree"){
+  
+  if(layout != "tree"){
+    citations <- list(df = Reduce(rbind,
+                                  lapply(citations, \(i) {
+                                    i$df
+                                  })),
+                      meta = Reduce(rbind,
+                                    lapply(citations, \(i) {
+                                      i$meta
+                                    }))) 
+  }
+  
+  citations$df <- citations$df[order(citations$meta$group),]
+
+  g <- graph_from_data_frame(citations$df)
+  citations$meta <- citations$meta[match(V(g)$name,
+                                         citations$meta$PMID),]
+
+  V(g)$citations <- citations$meta$citations+.2
+  V(g)$title <- citations$meta$title
+  V(g)$journal <- citations$meta$journal
+  V(g)$abstract <- citations$meta$abstract
+  V(g)$language <- citations$meta$language
+  V(g)$author <- citations$meta$author
+  V(g)$authorGroups <- citations$meta$group
+  V(g)$year <- citations$meta$year
+  V(g)$type <- citations$meta$type
+
+  if(layout == "tree"){
+    if(length(citations$df[citations$df$type == "cited","from"])>0){
+      gg <- create_layout(g,
+                          layout = layout_as_tree(g, 
+                                                  root = citations$df[citations$df$type == "cited","from"])
+      )
+    }else{
+      gg <- create_layout(g,
+                          layout = layout_as_tree(g, 
+                                                  root = citations$df[citations$df$type == "root","from"])
+      )
+    } 
+  }else{
+    gg <- create_layout(g,
+                        layout = layout_as_tree(g, 
+                                                mode = "all")
+    )
+  }
+  
+  gg$authorGroups <- as.factor(gg$authorGroups)
+  gg$authorGroups <- relevel(gg$authorGroups, ref = "none")
+
+  tooManyAuth <- strsplit(levels(gg$authorGroups), split = ",")
+
+  tooManyAuth <- lapply(tooManyAuth, \(x){
+    if(length(x)>2){
+      x[3] <- " et al."
+      return(x[1:3])
+    }
+    return(x)
+    })
+  levels(gg$authorGroups) <- sapply(tooManyAuth, paste0, collapse = ",")
+  gg$authorGroups <- relevel(gg$authorGroups, "none")
+  
+  gg
+}
+
+plotNet <- function(ggDat, 
+                    maxSize,
+                    nodeSize){
+  if (length(levels(ggDat$authorGroups)) > 10) {
+    x <- table(ggDat$authorGroups)
+    x <- x[names(x) != "none"]
+    x <- names(sort(x, decreasing = T)[1:10])
+    ggDat$authorGroups[!ggDat$authorGroups %in% x] <- "none"
+    ggDat$authorGroups <- ggDat$authorGroups |> as.character() |> as.factor()
+    ggDat$authorGroups <- relevel(ggDat$authorGroups, "none")
+  }
+  
+  gg <- ggraph(ggDat) + 
+    geom_edge_diagonal(aes(colour = type),
+                       show.legend = T, 
+                       arrow = arrow(length = unit(2, 'mm'))) + 
+    geom_node_point(aes(size = citations, 
+                        color = authorGroups,
+                        shape = type)) +
+    scale_size(breaks = seq(from = 1,
+                            to = maxSize,
+                            length.out = 3),
+               range = c(1,5)*nodeSize) +
+    ggsci::scale_color_igv() + 
+    theme_void() +
+    theme(legend.position = "right") +
+    guides(color = guide_legend(override.aes = list(edge_linetype = 0,
+                                                    size = 5)),
+           shape = guide_legend(override.aes = list(edge_linetype = 0,
+                                                    size = 5)),
+           size = guide_legend(override.aes = list(edge_linetype = 0)))
 }
